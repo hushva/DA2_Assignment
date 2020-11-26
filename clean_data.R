fda0027afda6eca47c4330648b85b1988906f313
hushva <71833817+hushva@users.noreply.github.com>
2020-11-26 23:57:51 +0100

    clean and merge done

--- clean_data.R
+++ clean_data.R
@@ -6,7 +6,7 @@ library(tidyverse)
 my_path <- "~/Documents/CEU/Courses/2020_Fall/Mandatory/DA2/DA2_Assignment/data/"
 
 # covid data
-cv <- read_csv(paste0(my_path,'raw/covid_09_11_2020_raw.csv'))
+cv <- read_csv(paste0(my_path,'raw/covid_02_11_2020_raw.csv'))
 
 # population data
 pop <- read_csv(paste0(my_path,'raw/pop_WDI_2019.csv'))

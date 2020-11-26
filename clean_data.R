c6a30f15b5c46824111448cab0a918095def4b04
hushva <71833817+hushva@users.noreply.github.com>
2020-11-26 23:37:14 +0100

    cleaned population

--- clean_data.R
+++ clean_data.R
@@ -38,3 +38,95 @@ cv2 <- cv2 %>% rename( country   = Country_Region ,
                        active    = Active_sum )
 
 glimpse( cv2 )
+
+
+
+#####################
+# Clean population data
+#
+
+glimpse(pop)
+
+## Check the observations:
+# 1) Filter out grouping observations based on using digits
+pop <- pop %>% filter( !grepl("[[:digit:]]", pop$iso2c) )
+
+
+# 2nd drop specific values
+drop_id <- c("EU","HK","OE")
+pop <- pop %>% filter( !grepl( paste( drop_id , collapse="|"), pop$iso2c ) )
+
+
+# 3rd drop values with certain starting character
+# Get the first letter from iso2c
+fl_iso2c <- substr(pop$iso2c, 1, 1)
+retain_id <- c("XK","ZA","ZM","ZW")
+
+# Filter out everything which starts X or Z except countries in retain_id
+pop <- pop %>% filter( !( grepl( "X", fl_iso2c ) | grepl( "Z", fl_iso2c ) & 
+                            !grepl( paste( retain_id , collapse="|"), pop$iso2c ) ) )
+
+rm( drop_id, fl_iso2c , retain_id )
+
+# Retain and rename variables which are going to be used later
+pop <-pop %>% transmute( country = country,
+                         population=SP.POP.TOTL )
+
+glimpse(pop)
+
+
+
+#####################
+# MERGE the two data table
+##
+
+
+df <- full_join(cv2,pop)
+
+# Correct some country names by hand
+use_name <- c("Congo, Rep.","Congo, Dem. Rep.","Czech Republic","Korea, Rep.","Kyrgyz Republic",
+              "Laos","St. Kitts and Nevis","St. Lucia","St. Vincent and the Grenadines",
+              "Slovak Republic","United States","Myanmar")
+
+alter_name <- c("Congo (Brazzaville)","Congo (Kinshasa)","Czechia","Korea, South","Kyrgyzstan",
+                "Lao PDR","Saint Kitts and Nevis","Saint Lucia","Saint Vincent and the Grenadines",
+                "Slovakia","US","Burma")
+
+# Simply use a for-cycle to change the name for the countries (note: ordering is important)
+for ( i in seq_along( use_name ) ){
+  df$country[ df$country == alter_name[ i ] ] <- use_name[ i ]
+}
+
+# Write a for-loop to find those which are partial or complete matches!
+# 1) auxillary table for countries without any population value
+aux <- df %>% filter( is.na(population) )
+# 2) Get the name of the countries
+countries_nm <- aux$country
+# 3) Iterate through all potential partial matches
+for ( i in seq_along( countries_nm ) ){
+  # Select those observations where partial match exists
+  log_select <- str_detect( df$country , countries_nm[ i ] )
+  # Get the population values for partial matches
+  c_partial <- df$population[ log_select ]
+  # If there is a match: only two countries are selected and one is missing the other has population:
+  if ( length( c_partial ) == 2 & sum( is.na( c_partial ) ) == 1 ){
+    # Replace the missing value with the match
+    df$population[ log_select & is.na(df$population)] = c_partial[ !is.na( c_partial ) ]
+    # Remove the replaced variable
+    df <- df %>% filter( !(log_select & is.na( df$confirmed ) ) )
+  }
+}
+
+# 4) Check the results:
+df %>% filter( is.na(population) )
+# These are:
+#   a) cruiser ships which stuck in national territory (Diamond Princess, MS Zaandam )
+#   b) disputed territories which are accepted by covid statistics but not by world bank 
+#       (Western Sahara, Taiwan or Kosovo)
+#   c) we have no population data on them (Ertirea, Holy See (Vatican))
+
+#####
+# Handle missing values:
+View( df %>% filter( !complete.cases(df) ) )
+# Drop if population, confirmed cases or death is missing
+df <- df %>% filter( !( is.na( population ) | is.na( confirmed ) | is.na( death ) ))
